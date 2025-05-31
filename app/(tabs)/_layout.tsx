7d3bdfdc6f3ead56f29f6ab68e87e9cada8ded0b
Seth Zathang <sethzathang@gmail.com>
2025-05-31 16:39:35 -0400

    fixed bottom tab issues

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome size={22} {...props}/>;
 }
 
 export default function TabLayout() {
